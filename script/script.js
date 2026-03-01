18cb800d1fa0210bf194e71e386ff86124e6da3e
Ded-Laptop <167631120+xHeidelberg@users.noreply.github.com>
2026-03-01 22:57:50 +0800

    Fixed User Interface for Calculator
    Added Display Holder
    Group the Number pad
    Group the Operator Pad

---
+++ script/script.js
@@ -0,0 +1,31 @@
+/*const input = document.querySelector('#sampleInput');
+const inputTwo = document.querySelector('#sampleInput2');
+const submit = document.querySelector('#submit');
+
+
+submit.addEventListener('click', () => {
+    const One = parseInt(input.value);
+    const Two = parseInt(inputTwo.value);
+    
+    console.log(One * Two);
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Cast
+let firstBatch = "";
+let secondBatch = "";
+let operatorCast = null;
+let tempPlaceholder = "";
